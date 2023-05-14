ee97ac01331eb8af7a5ca1451462d7bc267e1d48
dommadisreebalaji <sreebalajidommadi@gmail.com>
2023-05-14 11:34:27 +0000

    first-commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const NavBar = () => (
+  <nav className="nav-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
+      alt="nxt slides logo"
+      className="logo-image"
+    />
+    <h1 className="nxt-slides-heading">Nxt Slides</h1>
+  </nav>
+)
+
+export default NavBar
